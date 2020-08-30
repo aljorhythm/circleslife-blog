3840025b937e82bfa5b7184e567ac355e4db3596
Joel Lim <103879u@gmail.com>
2020-08-30 15:29:02 +0800

    set up gradle springboot project

---
+++ src/main/java/com/aljorhythm/blog/circleslifeblog/CircleslifeblogApplication.java
@@ -0,0 +1,13 @@
+package com.aljorhythm.blog.circleslifeblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleslifeblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircleslifeblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aljorhythm/blog/circleslifeblog/CircleslifeblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aljorhythm.blog.circleslifeblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircleslifeblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
